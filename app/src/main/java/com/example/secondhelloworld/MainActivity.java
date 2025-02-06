3bf4ebe58c52726b03e7e1869df0f93a2b06ec43
Adeliya <adeliya.karaivanova.22@gmail.com>
2025-02-06 21:04:32 +0300

    Hello world

---
+++ app/src/main/java/com/example/secondhelloworld/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.secondhelloworld;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        //TODO: complete this
+
+
+
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void onBtnClick(View view){
+        TextView textHello = findViewById(R.id.txtMessage);
+        EditText edtTxtName = findViewById(R.id.edtText);
+
+
+        //textHello.setText("Hello ".concat(edtTxtName.getText().toString()));
+        textHello.setText("Hello " + edtTxtName.getText().toString());
+    }
+
+}
